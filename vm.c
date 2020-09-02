4424c1f714e3244e56b475272026accb8e085035
sujay <sujay@pop-os.localdomain>
2020-09-02 12:01:30 +0530

    Added support for basic arithmetic operations using stack

--- vm.c
+++ vm.c
@@ -37,6 +37,14 @@ static InterpretResult run()
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
+#define BINARY_OP(op)     \
+    do                    \
+    {                     \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b);     \
+    } while (false)
+
     for (;;)
     {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -61,6 +69,23 @@ static InterpretResult run()
             break;
         }
 
+        case OP_ADD:
+            BINARY_OP(+);
+            break;
+        case OP_SUBTRACT:
+            BINARY_OP(-);
+            break;
+        case OP_MULTIPLY:
+            BINARY_OP(*);
+            break;
+        case OP_DIVIDE:
+            BINARY_OP(/);
+            break;
+
+        case OP_NEGATE:
+            push(-pop());
+            break;
+
         case OP_RETURN:
         {
             printValue(pop());
@@ -72,6 +97,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk)
