3a1c1b3b3ab6748fb40c7fc51baaf07ba9585190
sujay <sujay@pop-os.localdomain>
2020-10-10 01:14:36 +0530

    Added macros for converting C values to lox Value and checking that they are of the right type

--- value.h
+++ value.h
@@ -21,6 +21,18 @@ typedef struct
 
 } Value;
 
+// We need these macros to see that these values are of the approiate type
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+
 typedef struct
 {
     int capacity;
