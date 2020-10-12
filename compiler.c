55e6c49feb60e482374985134e40c7b5e8849c63
sujay <sujay@pop-os.localdomain>
2020-10-12 22:10:17 +0530

    Implemented functionality for true, false and nil types

--- compiler.c
+++ compiler.c
@@ -180,6 +180,24 @@ static void binary()
     }
 }
 
+static void literal()
+{
+    switch (parser.previous.type)
+    {
+    case TOKEN_FALSE:
+        emitByte(OP_FALSE);
+        break;
+    case TOKEN_NIL:
+        emitByte(OP_NIL);
+        break;
+    case TOKEN_TRUE:
+        emitByte(OP_TRUE);
+        break;
+    default:
+        return;
+    }
+}
+
 static void grouping()
 {
     expression();
@@ -207,6 +225,9 @@ static void unary()
     // Emit the operator instruction.
     switch (operatorType)
     {
+    case TOKEN_BANG:
+        emitByte(OP_NOT);
+        break;
     case TOKEN_MINUS:
         emitByte(OP_NEGATE);
         break;
@@ -227,7 +248,7 @@ ParseRule rules[] = {
     [TOKEN_SEMICOLON] = {NULL, NULL, PREC_NONE},
     [TOKEN_SLASH] = {NULL, binary, PREC_FACTOR},
     [TOKEN_STAR] = {NULL, binary, PREC_FACTOR},
-    [TOKEN_BANG] = {NULL, NULL, PREC_NONE},
+    [TOKEN_BANG] = {unary, NULL, PREC_NONE},
     [TOKEN_BANG_EQUAL] = {NULL, NULL, PREC_NONE},
     [TOKEN_EQUAL] = {NULL, NULL, PREC_NONE},
     [TOKEN_EQUAL_EQUAL] = {NULL, NULL, PREC_NONE},
@@ -241,17 +262,17 @@ ParseRule rules[] = {
     [TOKEN_AND] = {NULL, NULL, PREC_NONE},
     [TOKEN_CLASS] = {NULL, NULL, PREC_NONE},
     [TOKEN_ELSE] = {NULL, NULL, PREC_NONE},
-    [TOKEN_FALSE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FALSE] = {literal, NULL, PREC_NONE},
     [TOKEN_FOR] = {NULL, NULL, PREC_NONE},
     [TOKEN_FUN] = {NULL, NULL, PREC_NONE},
     [TOKEN_IF] = {NULL, NULL, PREC_NONE},
-    [TOKEN_NIL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_NIL] = {literal, NULL, PREC_NONE},
     [TOKEN_OR] = {NULL, NULL, PREC_NONE},
     [TOKEN_PRINT] = {NULL, NULL, PREC_NONE},
     [TOKEN_RETURN] = {NULL, NULL, PREC_NONE},
     [TOKEN_SUPER] = {NULL, NULL, PREC_NONE},
     [TOKEN_THIS] = {NULL, NULL, PREC_NONE},
-    [TOKEN_TRUE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_TRUE] = {literal, NULL, PREC_NONE},
     [TOKEN_VAR] = {NULL, NULL, PREC_NONE},
     [TOKEN_WHILE] = {NULL, NULL, PREC_NONE},
     [TOKEN_ERROR] = {NULL, NULL, PREC_NONE},
--- vm.c
+++ vm.c
@@ -54,6 +54,11 @@ static Value peek(int distance)
     return vm.stackTop[-1 - distance];
 }
 
+static bool isFalsey(Value value)
+{
+    return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
@@ -96,17 +101,36 @@ static InterpretResult run()
             break;
         }
 
+        case OP_NIL:
+            push(NIL_VAL);
+            break;
+
+        case OP_TRUE:
+            push(BOOL_VAL(true));
+            break;
+
+        case OP_FALSE:
+            push(BOOL_VAL(false));
+            break;
+
         case OP_ADD:
             BINARY_OP(NUMBER_VAL, +);
             break;
+
         case OP_SUBTRACT:
             BINARY_OP(NUMBER_VAL, -);
             break;
+
         case OP_MULTIPLY:
             BINARY_OP(NUMBER_VAL, *);
             break;
+
         case OP_DIVIDE:
             BINARY_OP(NUMBER_VAL, /);
+            break;
+
+        case OP_NOT:
+            push(BOOL_VAL(isFalsey(pop())));
             break;
 
         case OP_NEGATE:
